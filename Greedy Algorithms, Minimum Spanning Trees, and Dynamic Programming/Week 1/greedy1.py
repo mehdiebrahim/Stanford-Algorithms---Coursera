594d893014698bb116e12e22ef95f3ccd2e9cd26
mehdiebrahim <73022412+mehdiebrahim@users.noreply.github.com>
2020-12-12 18:57:10 +0000

    Update greedy1.py

--- Greedy Algorithms, Minimum Spanning Trees, and Dynamic Programming/Week 1/greedy1.py
+++ Greedy Algorithms, Minimum Spanning Trees, and Dynamic Programming/Week 1/greedy1.py
@@ -13,13 +13,13 @@ with open(f, 'r') as infile:
         diff.append((int(line[0])-int(line[1]),int(line[0]),int(line[1])))
         ratio.append((int(line[0])/int(line[1]),int(line[0]),int(line[1])))
 
-l = [(10,2),(10,1),(10,3),(20,3),(40,5),(100,2)]
-print(sorted(l,key= lambda x: (x[0],x[1])))
+
 diff = sorted(diff,key = lambda x:(x[0],x[1]))[::-1]
-ratio =  sorted(ratio,key = lambda x:(x[0],x[1]))[::-1]
+
 
 count = 0
 final = 0
 for elem in diff:
     count += elem[2]
     final += count*elem[1]
+final
