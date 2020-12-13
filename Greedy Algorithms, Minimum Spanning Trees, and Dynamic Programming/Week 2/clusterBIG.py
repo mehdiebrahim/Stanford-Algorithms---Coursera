6d3b83beeb4f9b38e2f0554206f525c29841be5d
mehdiebrahim <73022412+mehdiebrahim@users.noreply.github.com>
2020-12-13 17:42:03 +0000

    Add files via upload

---
+++ Greedy Algorithms, Minimum Spanning Trees, and Dynamic Programming/Week 2/clusterBIG.py
@@ -0,0 +1,136 @@
+a = [0,1,1,1,1,0,0,1,0,1,1,1,0,0,0,1,1,1,1,0,0,1,0,1]
+l = []
+count = 0
+# b= a.copy()
+import os
+from collections import defaultdict
+
+a = '0 1 1 0 0 0 0 0 1 1 0 1 0 0 1 1 1 1 0 0 1 1 1 1'
+
+
+def hamdist2(x):
+    x2 = x.split()
+    L = []
+    for i in range(24):
+        for j in range(i+1,24):
+
+            b= x2.copy()
+
+            one_i = 1^int(b[i])
+            zero_i = 0^int(b[i])
+
+            one_j = 1^int(b[j])
+            zero_j = 0^int(b[j])
+
+
+            if one_i == 1:
+                b[i] = '1'
+
+# #             elif zero_i==1:
+            else:
+#                 print(b[i],'bi')
+                b[i] = '0'
+
+            if one_j == 1:
+                b[j] = '1'
+
+            else:
+                b[j] = '0'
+            L.append(' '.join(b))
+    return L
+# print(hamdist2(a))
+
+def hamdist1(x):
+    x2 = x.split()
+    L = []
+    for i in range(24):
+        
+        b= x2.copy()
+        one_i = 1^int(b[i])
+        zero_i = 0^int(b[i])
+        
+        if one_i == 1:
+            b[i] = '1'
+            
+        else:
+            b[i] = '0'
+        L.append(' '.join(b))
+
+    return L
+
+def merged(clusters,cluster):
+    
+ 
+    set_of_clusters= []
+    merged = set()
+    for point in cluster:
+        cluster_f = find_cluster(clusters,point)
+
+
+    #     print(cluster_f)
+        if cluster_f:
+
+            if set_of_clusters:
+
+                last = set_of_clusters[-1]
+                if cluster_f not in set_of_clusters:
+                    set_of_clusters.append(cluster_f)
+                merged = cluster_f.union(last,merged)
+
+            else:
+                set_of_clusters.append(cluster_f)
+
+        else:
+            merged = merged.union(set([point]))
+    clusters.append(merged)
+    for i in set_of_clusters:
+        clusters.remove(i)
+    return clusters
+
+def find_cluster(clusters,point):
+    
+    for cluster in clusters:
+        if point in cluster:
+            return cluster
+        
+    return None
+# hamdist1('0 1 1 0 0 0 0 0 1 1 0 1 0 0 1 1 1 1 0 0 1 1 1 1')
+os.chdir('/Users/mehdiebrahim/Desktop/Stanford Algorithms - Data')
+d = 'cluster2.txt'
+
+with open(d, 'r') as infile:
+    '''opens file and puts each row into adjMat'''
+    no_of_nodes,no_of_bits,nodes = 0,0,defaultdict(list)
+    for line in infile.readlines():
+        line = line.split()
+
+        line = ' '.join(line)
+        
+        if len(line.split())==2:
+            no_of_nodes,no_of_bits = int(line[0]),int(line[1])
+        else:
+#            line = list(map(lambda x: int(x),line))
+            nodes[line].append(line)
+
+list_of_codes = [code for code in nodes]
+clusters = []
+
+
+while list_of_codes:
+    code = list_of_codes.pop(0)
+    initial = nodes[code]
+    neighbours = hamdist1(code) + hamdist2(code)
+#     print(len(hamdist1(code)),len(hamdist2(code)))
+    cluster = set(nodes[code])
+    counts = []
+    for neighbour in neighbours:
+       
+        if neighbour in nodes:
+            
+            cluster.add(neighbour)
+    clusters = merged(clusters,cluster)
+
+
+            
+print(len(clusters))           
+    
